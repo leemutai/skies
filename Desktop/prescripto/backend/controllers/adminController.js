3174c9587b4751c935a2b0a7d3b72d2860384b30
leemutai <mutailee361@gmail.com>
2024-10-13 23:23:38 +0300

    configured the api calls

---
+++ Desktop/prescripto/backend/controllers/adminController.js
@@ -0,0 +1,18 @@
+
+
+
+// api for adding doctor
+const addDoctor = async ()=> {
+    try{
+        const {name,email,password,image,speciality,degree,experience,about,fees,address} = req.body;
+        const imageFile = req.file
+
+        console.log({name,email,password,image,speciality,degree,experience,about,fees,address},imageFile)
+
+
+    } catch (error) {
+         
+    }
+}
+
+export {addDoctor};
--- Desktop/prescripto/backend/server.js
+++ Desktop/prescripto/backend/server.js
@@ -3,10 +3,12 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB   from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js'; // Correctly imports the default export
+
 
 // app config
 const app = express();
-const port = process.env.port || 4000
+const port = process.env.PORT || 4000
 connectDB();
 connectCloudinary();
 
@@ -15,9 +17,12 @@ app.use(express.json());
 app.use(cors());
 
 // api routes/endpoint
+app.use('/api/admin',adminRouter)
+
+// localhost:4000/api/admin/add-doctor
 
 app.get('/',(req,res)=> {
     res.send('API WORKING')
-})
+});
 
-app.listen(port,()=> console.log("Server Started",port))
+app.listen(port,()=> console.log("Server Started on",port))
