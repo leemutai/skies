f031c17608a95aa764a3fcef4b15d4b66f65ab4c
leemutai <mutailee361@gmail.com>
2024-10-15 23:31:10 +0300

    Implemented admin login authentication with JWT and fixed doctor addition API

--- Desktop/prescripto/backend/controllers/adminController.js
+++ Desktop/prescripto/backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bcrypt  from 'bcrypt';
 import  { v2 as cloudinary } from 'cloudinary';
 import doctorModel from '../models/doctorModels.js';
+import jwt from 'jsonwebtoken';
 
 
 // api for adding doctor
@@ -61,4 +62,25 @@ const addDoctor = async (req,res)=> {
     }
 }
 
-export {addDoctor};
+//api for the admin login
+
+const loginAdmin = async (req,res)=> {
+   try {
+      const {email,password} = req.body;
+      if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+
+         const token = jwt.sign(email+password,process.env.JWT_SECRET);
+         res.json({success:true,token});
+
+      } else {
+         res.json({success:false,message:"Invalid credentials"});
+
+      }
+
+   } catch (error) {
+      console.log(error);
+         res.json({success:false,message:error.message});
+   }
+}
+
+export {addDoctor,loginAdmin};
