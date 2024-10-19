505ae7cfcf4f134db68d44831ad76e86e584e0b4
leemutai <mutailee361@gmail.com>
2024-10-19 23:09:14 +0300

    Improved login form functionality with toast notifications for errors and success, fixed input handling for email and password, and added server-side error messages display.

--- Desktop/prescripto/admin/src/pages/Login.jsx
+++ Desktop/prescripto/admin/src/pages/Login.jsx
@@ -1,9 +1,37 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAToken, backendurl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (state === "Admin") {
+        const { data } = await axios.post(backendurl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span>Login
@@ -11,6 +39,8 @@ const Login = () => {
         <div className="w-full">
           <p>Email</p>
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="email"
             required
@@ -19,6 +49,8 @@ const Login = () => {
         <div className="w-full">
           <p>Password</p>
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="password"
             required
