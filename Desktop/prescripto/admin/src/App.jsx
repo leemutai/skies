71dd9bd1b9fa40a93c0aed22ce975515641ef416
leemutai <mutailee361@gmail.com>
2024-10-19 12:44:38 +0300

    reisnated the admin folder

--- Desktop/prescripto/admin/src/App.jsx
+++ Desktop/prescripto/admin/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Login from "./pages/login";
 
 const App = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  return <div className="text-green-500">App</div>;
 };
 
 export default App;
--- Desktop/prescripto/admin/src/main.jsx
+++ Desktop/prescripto/admin/src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import AdminContextProvider from "./context/AdminContext.jsx";
-import DoctorContextProvider from "./context/DoctorContext.jsx";
-import AppContextProvider from "./context/AppContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AdminContextProvider>
-      <DoctorContextProvider>
-        <AppContextProvider>
-          <App />
-        </AppContextProvider>
-      </DoctorContextProvider>
-    </AdminContextProvider>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
