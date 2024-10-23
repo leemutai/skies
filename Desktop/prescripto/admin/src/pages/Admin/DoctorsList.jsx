4a4163a9a957cbce5b85aad706775279057cdff8
leemutai <mutailee361@gmail.com>
2024-10-24 01:31:59 +0300

    Fetch and display doctors list with hover effects, added context integration for fetching data from API, and refined UI transitions for doctor cards.

--- Desktop/prescripto/admin/src/pages/Admin/DoctorsList.jsx
+++ Desktop/prescripto/admin/src/pages/Admin/DoctorsList.jsx
@@ -10,8 +10,33 @@ const DoctorsList = () => {
     }
   }, [aToken]);
 
-  return;
-  <div></div>;
+  return (
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium">All Doctors</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {doctors.map((item, index) => (
+          <div
+            className="group border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer hover:shadow-lg transition-all duration-500"
+            key={index}
+          >
+            <img
+              className="bg-indigo-50 group-hover:bg-primary transition-all duration-500"
+              src={item.image}
+              alt=""
+            />
+            <div className="p-3">
+              <p className="font-medium">{item.name}</p>
+              <p className="text-sm text-gray-500">{item.speciality}</p>
+              <div className="flex items-center gap-2 mt-2">
+                <input type="checkbox" checked={item.available} readOnly />
+                <p>Available</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DoctorsList;
