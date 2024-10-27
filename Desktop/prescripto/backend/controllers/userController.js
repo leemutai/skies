c9a6fdd1d898af17011048e2990972ef303ccafe
leemutai <mutailee361@gmail.com>
2024-10-27 12:22:24 +0300

    created schema for appoinment

--- Desktop/prescripto/backend/controllers/userController.js
+++ Desktop/prescripto/backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bcrypt from 'bcrypt';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import {v2 as cloudinary} from 'cloudinary';
 
 // API TO register user
 const registerUser = async(req,res)=>{
@@ -96,4 +97,33 @@ const getProfile = async (req,res)=>{
 
 }
 
-export {registerUser,loginUser,getProfile};
+// API TO UPDATE USER PROFILE
+const updateProfile = async (req,res)=>{
+    try {
+        const {userId,name,phone,address,dob,gender} = req.body;
+        const imageFile = req.file;
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({success:false,message:"Data Missing"});
+            
+        }
+
+        await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender})
+
+        if (imageFile) {
+            // upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+            const imageUrl = imageUpload.secure_url;
+
+            await userModel.findByIdAndUpdate(userId,{image:imageUrl}) 
+        }
+
+        res.json({success:true,message:"Profile Updated"});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
+}
+
+export {registerUser,loginUser,getProfile,updateProfile};
