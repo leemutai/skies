e49ed9e221a98360570c0a01afaade837ec119ac
leemutai <mutailee361@gmail.com>
2024-11-11 12:10:36 +0300

    Add doctor login and availability toggle functionality with secure JWT authentication

--- Desktop/prescripto/backend/controllers/doctorController.js
+++ Desktop/prescripto/backend/controllers/doctorController.js
@@ -1,5 +1,6 @@
 import doctorModel from "../models/doctorModels.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 const changeAvailability =  async (req,res)=> {
@@ -33,4 +34,33 @@ const doctorList = async (req,res)=> {
     }
 }
 
-export {changeAvailability,doctorList};
+// API for doctor login
+const loginDoctor = async (req,res)=> {
+    try {
+
+        const {email,password} = req.body;
+        const doctor = await doctorModel.findOne({email});
+
+        if (!doctor) {
+            return res.json({success:false,message:"Invalid credentials"});
+            
+        }
+
+        const isMatch = await bcrypt.compare(password,doctor.password);
+
+        if (isMatch) {
+            const token = jwt.sign({id:doctor._id},process.env.JWT_SECRET);
+
+            res.json({success:true,token})
+            
+        } else {
+            res.json({success:false,message:"Invalid credentials"});
+        }
+        
+    } catch (error) {
+        console.log(error);
+      res.json({success:false,message:error.message});
+    }
+}
+
+export {changeAvailability,doctorList,loginDoctor};
