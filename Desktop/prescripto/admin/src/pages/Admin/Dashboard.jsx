c72780ef0bdd09467be76a6779bd6e4f4b81b336
leemutai <mutailee361@gmail.com>
2024-11-11 11:26:13 +0300

    Enhance dashboard with appointment list, cancellation option, and date formatting

--- Desktop/prescripto/admin/src/pages/Admin/Dashboard.jsx
+++ Desktop/prescripto/admin/src/pages/Admin/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
 import { assets } from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
   const { aToken, getDashData, cancelAppointment, dashData } =
     useContext(AdminContext);
+
+  const { slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
     if (aToken) {
@@ -15,29 +18,76 @@ const Dashboard = () => {
   return (
     dashData && (
       <div className="m-5">
-        <div>
-          <div>
-            <img src={assets.doctor_icon} alt="" />
+        <div className="flex flex-wrap gap-3">
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.doctor_icon} alt="" />
             <div>
-              <p>{dashData.doctors}</p>
-              <p>Doctors</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.doctors}
+              </p>
+              <p className="text-gray-400">Doctors</p>
             </div>
           </div>
 
-          <div>
-            <img src={assets.appointment_icon} alt="" />
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.appointment_icon} alt="" />
             <div>
-              <p>{dashData.appointments}</p>
-              <p>Appointments</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.appointments}
+              </p>
+              <p className="text-gray-400">Appointments</p>
             </div>
           </div>
 
-          <div>
-            <img src={assets.patients_icon} alt="" />
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.patients_icon} alt="" />
             <div>
-              <p>{dashData.patients}</p>
-              <p>Patients</p>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.patients}
+              </p>
+              <p className="text-gray-400">Patients</p>
             </div>
+          </div>
+        </div>
+
+        <div className="bg-white">
+          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border">
+            <img src={assets.list_icon} alt="" />
+            <p className="font-semibold">Latest Booking</p>
+          </div>
+          <div className="pt-4 border border-t-0">
+            {dashData.latestAppointments.map((item, index) => (
+              <div
+                className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100"
+                key={index}
+              >
+                <img
+                  className="rounded-full w-10"
+                  src={item.docData.image}
+                  alt=""
+                />
+                <div className="flex-1 text-sm">
+                  <p className="text-gray-800 font-medium">
+                    {item.docData.name}
+                  </p>
+                  <p className="text-gray-600">
+                    {slotDateFormat(item.slotDate)}
+                  </p>
+                </div>
+                {item.cancelled ? (
+                  <p className="text-red-400 text-xs font-medium">Cancelled</p>
+                ) : (
+                  <div className="flex justify-center">
+                    <img
+                      onClick={() => cancelAppointment(item._id)}
+                      className="w-8 h-8 cursor-pointer"
+                      src={assets.cancel_icon}
+                      alt="Cancel"
+                    />
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
