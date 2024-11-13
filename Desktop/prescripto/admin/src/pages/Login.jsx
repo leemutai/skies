66cc63b1b749763ceefece733a7261b623f4f992
leemutai <mutailee361@gmail.com>
2024-11-13 12:14:48 +0300

    Implement login flows for Admin and Doctor with token handling in context providers,Created DoctorContextProvider to handle doctor tokens (dToken), Implemented AdminContextProvider for admin token (aToken) management,Enhanced Login component to support toggling between Admin and Doctor login,Fixed token storage in localStorage and verified console logs for tokens

--- Desktop/prescripto/admin/src/pages/Login.jsx
+++ Desktop/prescripto/admin/src/pages/Login.jsx
@@ -2,20 +2,22 @@ import React, { useContext, useState } from "react";
 import { AdminContext } from "../context/AdminContext";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { setAToken, backendurl } = useContext(AdminContext);
+  const { setAToken, backendUrl } = useContext(AdminContext);
+  const { setDToken } = useContext(DoctorContext);
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
 
     try {
       if (state === "Admin") {
-        const { data } = await axios.post(backendurl + "/api/admin/login", {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
           email,
           password,
         });
@@ -26,6 +28,17 @@ const Login = () => {
           toast.error(data.message);
         }
       } else {
+        const { data } = await axios.post(backendUrl + "/api/doctor/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("dToken", data.token);
+          setDToken(data.token);
+          console.log(data.token);
+        } else {
+          toast.error(data.message);
+        }
       }
     } catch (error) {}
   };
