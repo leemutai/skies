9d8b14810f52c20d8cb12c9e8da79433d352003d
leemutai <mutailee361@gmail.com>
2024-11-15 22:25:47 +0300

    Added responsive layout adjustments for mobile view.,Integrated date and currency formatting functions for appointment details.,Styled payment badges and added action icons for managing appointments.

--- Desktop/prescripto/admin/src/pages/Doctor/DoctorAppointments.jsx
+++ Desktop/prescripto/admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
 
 const DoctorAppointments = () => {
   const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+  const { calculateAge, slotDateFormat, currency } = useContext(AppContext);
 
   useEffect(() => {
     if (dToken) {
@@ -24,6 +27,49 @@ const DoctorAppointments = () => {
           <p>Fees</p>
           <p>Action</p>
         </div>
+
+        {/* Appointment Rows */}
+        {appointments.map((item, index) => (
+          <div
+            className="flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50"
+            key={index}
+          >
+            <p className="max-sm:hidden">{index + 1}</p>
+            <div className="flex items-center gap-2">
+              <img
+                src={item.userData.image}
+                alt=""
+                className="w-8 h-8 rounded-full"
+              />
+              <p>{item.userData.name}</p>
+            </div>
+            <div>
+              <p className="text-xs inline border border-primary px-2 rounded-full">
+                {item.payment ? "Online" : "CASH"}
+              </p>
+            </div>
+            <p className="max-sm:hidden">{calculateAge(item.userData.dob)}</p>
+            <p>
+              {slotDateFormat(item.slotDate)},{item.slotTime}
+            </p>
+            <p>
+              {currency}
+              {item.amount}
+            </p>
+            <div className="flex">
+              <img
+                className="w-10 cursor-pointer"
+                src={assets.cancel_icon}
+                alt=""
+              />
+              <img
+                className="w-10 cursor-pointer"
+                src={assets.tick_icon}
+                alt=""
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
