0b04b0b17786cc90e3e8d0f0c1b7cc074b40d8e2
leemutai <mutailee361@gmail.com>
2024-11-19 12:18:38 +0300

    Enhance DoctorDashboard and Implement Doctor Profile APIs,Improved the DoctorDashboard component with dynamic rendering for recent appointments and interactive cancel/complete actions.,Created a createDoctorProfile API to allow creating doctor profiles.,Added an updateDoctorProfile API to update doctor details based on ID. Refined error handling and added status-based responses for the new APIs.

--- Desktop/prescripto/admin/src/pages/Doctor/DoctorProfile.jsx
+++ Desktop/prescripto/admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { DoctorContext } from "../../context/DoctorContext";
+import { AppContext } from "../../context/AppContext";
 
-function DoctorProfile() {
+const DoctorProfile = () => {
+  const { dToken, profileData, setProfileData, getProfileData } =
+    useContext(DoctorContext);
+  const { currency, backendUrl } = useContext(AppContext);
+
+  useEffect(() => {
+    if (dToken) {
+      getProfileData();
+    }
+  }, [dToken]);
   return <div></div>;
-}
+};
 
 export default DoctorProfile;
