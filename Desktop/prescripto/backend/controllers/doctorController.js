e252e58972176394514df242a3a81d11a1a332c4
leemutai <mutailee361@gmail.com>
2024-11-20 03:16:35 +0300

    Updated doctor availability indicator with dynamic status styling

--- Desktop/prescripto/backend/controllers/doctorController.js
+++ Desktop/prescripto/backend/controllers/doctorController.js
@@ -163,4 +163,44 @@ const doctorDashboard = async (req,res)=> {
         
     }
 }
-export {changeAvailability,doctorList,loginDoctor,appointmentsDoctor , appointmentCancel, appointmentComplete,doctorDashboard};
+
+// API to get doctor profie for doctor panel
+
+const doctorProfile = async (req,res)=> {
+    try {
+
+        const {docId} = req.body;
+        const profileData = await doctorModel.findById(docId).select('-password');
+        res.json({success:true,profileData});
+        
+    } catch (error) {
+        console.log(error);
+      res.json({success:false,message:error.message});
+    }
+}
+
+// API to update doctor profile for doctor panel
+const updateDoctorProfile = async (req,res)=> {
+    try {
+
+        const {docId,fees,address,available} = req.body;
+        await doctorModel.findByIdAndUpdate(docId,{fees,address,available});
+        res.json({success:true,message:"Profile updated successfully"});
+        
+    } catch (error) {
+        console.log(error);
+      res.json({success:false,message:error.message});
+    }
+}
+
+
+export {changeAvailability,
+    doctorList,
+    loginDoctor,
+    appointmentsDoctor , 
+    appointmentCancel,
+     appointmentComplete,
+     doctorDashboard,
+     doctorProfile,
+     updateDoctorProfile,
+    };
